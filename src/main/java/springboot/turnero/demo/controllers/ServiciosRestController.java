a4e25f116483228d3bc08618bd36e83ca5fe4233
Efrain <javierefrainrolando@gmail.com>
2026-01-17 20:37:47 -0300

    Avances

--- src/main/java/springboot/turnero/demo/controllers/ServiciosRestController.java
+++ src/main/java/springboot/turnero/demo/controllers/ServiciosRestController.java
@@ -5,19 +5,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/api") 
 public class ServiciosRestController {
 
     @GetMapping("/detalleServicio_info2")
-    public String home() {
+    public Map<String, Object> home() {
         Map<String, Object> model = new HashMap<>();
         model.put("Titulo", "Cortes de Cabello");
         model.put("Servidor", "Servicios Disponibles");
+        model.put("Descripcion", "Servicios de Corte de Cabello");
+        model.put("Precio", "$500");
+        model.put("Duracion", "30 minutos");
 
-        return "detalleServicio_info2";
+        return model;
     }
 
 }
